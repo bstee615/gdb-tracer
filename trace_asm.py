6d61593e3e48f8c80ddf8ed4aec6718062afd238
bstee615 <benjaminjsteenhoek@gmail.com>
2022-02-08 03:43:25 -0600

    Use file argument for trace-asm. Flush stdout before checking pipe file. Escape XML characters.

--- trace_asm.py
+++ trace_asm.py
@@ -1,6 +1,7 @@
 # https://stackoverflow.com/a/46661931/8999671
 import os
 import traceback
+import re
 
 should_stop = False
 
@@ -17,61 +18,81 @@ class TraceAsm(gdb.Command):
         should_stop = False
     def invoke(self, argument, from_tty):
         argv = gdb.string_to_argv(argument)
-        if argv:
-            gdb.write('Does not take any arguments.\n')
-        else:
-            n_instr = 1000000
-            text_offset = 0
-            with open('log.xml', 'w') as f:
-                f.write('<trace>\n')
-                for _ in range(n_instr):
-                    if should_stop:
-                        f.write("stopped{}".format(os.linesep))
-                        break
-                    try:
-                        frame = gdb.selected_frame()
-                        sal = frame.find_sal()
-                        symtab = sal.symtab
-                        path = None
-                        line = None
-                        if symtab:
-                            path = symtab.fullname()
-                            line = sal.line
+        
+        n_instr = 1000000
+        text_offset = 0
+        with open(argv[0] + '_log', 'w') as f:
+            f.write('<trace>\n')
+            for _ in range(n_instr):
+                if should_stop:
+                    break
+                try:
+                    frame = gdb.selected_frame()
+                    sal = frame.find_sal()
+                    symtab = sal.symtab
+                    path = None
+                    line = None
+                    if symtab:
+                        path = symtab.fullname()
+                        line = sal.line
 
-                        is_main_exe = path is not None and path.startswith('/workspace')
-                        if is_main_exe:
-                            # Navigating scope blocks to gather variables https://stackoverflow.com/a/30032690/8999671
-                            block = frame.block()
-                            variables = {}
-                            while block:
-                                for symbol in block:
-                                    if (symbol.is_argument or symbol.is_variable):
-                                        name = symbol.name
-                                        if not name in variables and not name.startswith('std::'):
-                                            value = symbol.value(frame)
-                                            variables[name] = str(value)
-                                block = block.superblock
-                            f.write(f'<program_point filename="{path}" line="{line}">\n')
-                            for name, value in variables.items():
-                                f.write(f'<variable name="{name}">{value}</variable>\n')
-                            f.write('</program_point>\n')
-                    except Exception as e:
-                        f.write("exception {}{}".format(e, os.linesep))
-                        traceback.print_exc()
-                    # TODO: May want to use named pipes for smarter solution
-                    if os.path.exists('tmp.txt'):
-                        with open('tmp.txt') as inf:
-                            inf.seek(text_offset)
-                            text = inf.read()
-                            text_offset += len(text)
-                        if not text:
+                    is_main_exe = path is not None and path.startswith('/workspace')
+                    if is_main_exe:
+                        # Navigating scope blocks to gather variables https://stackoverflow.com/a/30032690/8999671
+                        block = frame.block()
+                        variables = {}
+                        while block:
+                            for symbol in block:
+                                if (symbol.is_argument or symbol.is_variable):
+                                    name = symbol.name
+                                    if not name in variables and not name.startswith('std::'):
+                                        value = str(symbol.value(frame))
+                                        valid = True
+                                        # if value.startswith('<') and value.endswith('>'):
+                                        #     value = value[1:-1]
+                                        #     valid = False
+                                        # value = re.sub(r'\<([^>]*)\>', '"\1"', value)
+                                        value = (value
+                                            .replace('&', '&amp;')
+                                            .replace('<', '&lt;')
+                                            .replace('>', '&gt;')
+                                            )
+                                        variables[name] = (valid, value)
+                            block = block.superblock
+                        f.write(f'<program_point filename="{path}" line="{line}">\n')
+                        for name, (valid, value) in variables.items():
+                            f.write(f'<variable name="{name}" valid="{"true" if valid else "false"}">{value}</variable>\n')
+
+                        # TODO: May want to use named pipes for smarter solution
+                        gdb.execute('call fflush(stdout)')
+                        if os.path.exists(argv[0]):
+                            print('read', argv[0], 'at', text_offset)
+                            with open(argv[0]) as inf:
+                                inf.seek(text_offset)
+                                text = inf.read()
+                                if len(text) > 0:
+                                    print('Got text')
+                                text_offset += len(text)
+                            if not text:
+                                text = None
+                        else:
                             text = None
-                    else:
-                        text = None
-                    if text is not None:
-                        f.write(f'<stdout>{text}</stdout>\n')
-                    gdb.execute('s', to_string=True)  # This line steps to the next line which reduces overhead, but skips some lines compared to stepi.
-                f.write('</trace>\n')
+                        if text:
+                            f.write(f'<stdout>{text}</stdout>\n')
+                        f.write('</program_point>\n')
+                except Exception as e:
+                    # f.write("exception {}{}".format(e, os.linesep))
+                    traceback.print_exc()
+
+                # gdb.execute('s', to_string=True)  # This line steps to the next line which reduces overhead, but skips some lines compared to stepi.
+                # gdb.execute('si', to_string=True)  # Too slow
+                gdb.execute('n', to_string=True)  # Juuuust right... until we have to step into a function call.
+            with open(argv[0]) as inf:
+                inf.seek(text_offset)
+                text = inf.read()
+            if text:
+                f.write(f'<stdout unattached="true">{text}</stdout>\n')
+            f.write('</trace>\n')
 def exit_handler(event):
     # check the type of stop, the following is the common one after step/next,
     # a more complex one would be a subclass (for example breakpoint or signal)
